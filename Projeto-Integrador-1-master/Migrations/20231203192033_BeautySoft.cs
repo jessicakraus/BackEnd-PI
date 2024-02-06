c5f23b8d366197dd6cb3a6ba5e3033ee350d1df4
Jessica Kraus <jessica_kraus@outlook.com>
2024-02-06 18:13:14 -0300

    Backend

---
+++ Projeto-Integrador-1-master/Migrations/20231203192033_BeautySoft.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Beautysoft.Migrations
+{
+    /// <inheritdoc />
+    public partial class BeautySoft : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Tempo",
+                table: "Procedimentos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Usuarios");
+
+            migrationBuilder.DropColumn(
+                name: "Tempo",
+                table: "Procedimentos");
+        }
+    }
+}
---
+++ Projeto-Integrador-1-master/Migrations/20231203223000_UpdateDB.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Beautysoft.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Agendamentos_Procedimentos_ProcedimentoId",
+                table: "Agendamentos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Agendamentos_ProcedimentoId",
+                table: "Agendamentos");
+
+            migrationBuilder.RenameColumn(
+                name: "ProcedimentoId",
+                table: "Agendamentos",
+                newName: "TipoProcedimento");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Descricao",
+                table: "Agendamentos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nome",
+                table: "Agendamentos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Tempo",
+                table: "Agendamentos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Valor",
+                table: "Agendamentos",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Descricao",
+                table: "Agendamentos");
+
+            migrationBuilder.DropColumn(
+                name: "Nome",
+                table: "Agendamentos");
+
+            migrationBuilder.DropColumn(
+                name: "Tempo",
+                table: "Agendamentos");
+
+            migrationBuilder.DropColumn(
+                name: "Valor",
+                table: "Agendamentos");
+
+            migrationBuilder.RenameColumn(
+                name: "TipoProcedimento",
+                table: "Agendamentos",
+                newName: "ProcedimentoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Agendamentos_ProcedimentoId",
+                table: "Agendamentos",
+                column: "ProcedimentoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Agendamentos_Procedimentos_ProcedimentoId",
+                table: "Agendamentos",
+                column: "ProcedimentoId",
+                principalTable: "Procedimentos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
